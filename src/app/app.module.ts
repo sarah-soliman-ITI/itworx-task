b7ce02b7f9fdace0328760b8c1bbcb14f330775f
NeomTech -1 <NeomTech-1@WaleedBecare.onmicrosoft.com>
2021-01-26 18:25:27 +0200

    add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './components/courses/courses.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule
