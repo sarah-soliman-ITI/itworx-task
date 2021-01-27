46cc01b3955e057a22b89156f2812b74c2e5dade
NeomTech -1 <NeomTech-1@WaleedBecare.onmicrosoft.com>
2021-01-27 14:59:24 +0200

    course list

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -19,14 +19,23 @@ export class CoursesComponent implements OnInit {
   constructor(private httpClient : HttpClient) { }
 
   ngOnInit(): void {
-    this.httpClient.get("assets/lists/requests.json").subscribe(data =>{
+    this.httpClient.get("assets/lists/courses.json").subscribe(data =>{
       console.log(data);
       this.courses = data;
-      var coursesList = this.courses.filter(x => x.Courses != null || 
-        x.Courses != 0).map(x => x.Courses);
-        this.coursesListBind = coursesList;
+      // var coursesList = this.courses.filter(x => x.Courses != null || 
+      //   x.Courses != 0).map(x => x.Courses);
+      //   this.coursesListBind = coursesList;
 
     })
+
+  //   "CourseId": 123,
+  //   "CourseName": "Course 1",
+  //   "CoursePrice": 123.5,
+  //   "AvailableSeats": 25,
+  //   "CourseImg": "https://cmkt-image-prd.freetls.fastly.net/0.1.0/ps/6772890/600/400/m2/fpnw/wm0/9-.jpg?1564910091&s=cae782a267bc92f8a563993cb79de957",
+	// "courseLevel":"Beginner",
+	// "CourseDuration":40,
+	// "CourseCategory":"IT & Software"
    
   
   }
